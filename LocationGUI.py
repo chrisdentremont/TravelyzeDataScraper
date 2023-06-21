79ea469296869dc4730131bd443a0db7b373c2cb
dentremontcatwit <dentremontc@wit.edu>
2023-06-21 19:58:11 -0400

    Cleaned and documented code

--- LocationGUI.py
+++ LocationGUI.py
@@ -3,17 +3,19 @@ from tkinter import ttk
 from tkinter import messagebox
 import requests
 from bs4 import BeautifulSoup
-import os
 import openai
 import threading
 
 openai.api_key = ""
 
+
 def submitLocation():
+    """Starts a new thread to """
     threading.Thread(target=generateSummaries).start()  
 
 
 def generateSummaries():
+    # Retrieve the entered URL, send a GET request to get HTML content and pass it to BeautifulSoup to parse through the HTML
     locationURL = URLentry.get()
     page = requests.get(locationURL)
     soup = BeautifulSoup(page.content, "html.parser")
@@ -24,7 +26,7 @@ def generateSummaries():
         messagebox.showerror("Invalid Webpage", "This webpage is not from Wikipedia - please enter a Wikipedia URL.")
         return
         
-
+    # If URL is Wikipedia link, display progress bar/label and clear output text
     outputSummary.delete("1.0", "end")
     progressBar.pack()
     progressBar.start()
@@ -33,13 +35,16 @@ def generateSummaries():
 
     summaryGroup = ""
 
-
+    # Find every <p> element in HTML and have the OpenAI API summarize it to clean and shorten text. (Temporary, will flesh it out)
     pTexts = soup.find_all("p")
     completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages = [
         {"role": "system", "content" : "Make a short summary of this text: " + pTexts[1].text}
         ])
+    
+    # Add the resulting text to the output string to display in the output text area
     summaryGroup += f"Before GPT =======================================================\n\n {pTexts[1].text} \n\n After GPT =======================================================\n\n {completion.choices[0].message.content} \n\n"
-    count = 0
+    
+    # count = 0
 
     # for element in pTexts:
     #     if cancelEvent.is_set():
@@ -60,14 +65,18 @@ def generateSummaries():
     #     # count += 1
     #     # if count > 10: break 
     
+    # Display the final output and clear the progress bar/label
     outputSummary.insert(tk.END, summaryGroup)
     progressBar.stop()
     progressLabel['text'] = ""
     progressBar.pack_forget()
 
-window = tk.Tk(className="Location Data Entry")
+# Main window widget
+window = tk.Tk()
+window.title("Travelyze Data Entry Tool")
 window.geometry("800x600")
 
+# Widgets to display text box to enter webpage URL
 URLframe = tk.Frame(master=window)
 URLframe.pack()
 URLlabel = tk.Label(master=URLframe, text="Enter Wikipedia URL:", font=("Arial", 16))
@@ -77,12 +86,14 @@ URLentry.pack(padx=50, pady=(0, 10))
 URLbutton = tk.Button(text="Submit", font=("Arial", 12), command=submitLocation)
 URLbutton.pack(pady=(0, 30))
 
+# Widgets to display progress bar for submitting URL
 progressFrame = tk.Frame(master=window)
 progressFrame.pack()
 progressLabel = tk.Label(master=progressFrame, font="Arial")
 progressLabel.pack(pady=10)
 progressBar = ttk.Progressbar(progressFrame, mode='indeterminate', length=280)
 
+# Widgets to display text area to show output of web scraping
 outputFrame = tk.Frame(master=window)
 outputFrame.pack()
 outputSummary = tk.Text(master=outputFrame, font="Arial", height=500)

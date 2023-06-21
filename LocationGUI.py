d4a4672bc7e7612659a9f0367a6ed61fdc156e3f
dentremontcatwit <dentremontc@wit.edu>
2023-06-21 17:43:43 -0400

    Basic web scrape and OpenAI implementation

--- LocationGUI.py
+++ LocationGUI.py
@@ -1,63 +1,69 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 import requests
 from bs4 import BeautifulSoup
 import os
 import openai
 import threading
-from threading import Event
 
 openai.api_key = ""
 
-cancelEvent = Event()
-
 def submitLocation():
-    cancelEvent = Event()
+    threading.Thread(target=generateSummaries).start()  
 
-    URLbutton.config(text="Cancel", command=cancelGeneration)
 
-    threading.Thread(target=generateSummaries, args=(cancelEvent,)).start()  
-    
-def cancelGeneration():
-    cancelEvent.set()
-    global progressLabel
-    progressLabel['text'] = ""
-    progressBar.stop()
-    URLbutton.config(text="Submit", command=submitLocation)
-
-def generateSummaries(cancelEvent):
-    global progressLabel
-    progressLabel['text'] = "Generating summaries..."
+def generateSummaries():
     locationURL = URLentry.get()
     page = requests.get(locationURL)
     soup = BeautifulSoup(page.content, "html.parser")
 
-    pTexts = soup.find_all("p")
-    count = 0
+    #Only accept Wikipedia URLs
+    pageTitle = soup.find("title")
+    if "Wikipedia" not in pageTitle.text:
+        messagebox.showerror("Invalid Webpage", "This webpage is not from Wikipedia - please enter a Wikipedia URL.")
+        return
+        
+
+    outputSummary.delete("1.0", "end")
+    progressBar.pack()
+    progressBar.start()
+    global progressLabel
+    progressLabel['text'] = "Generating summaries..."
 
     summaryGroup = ""
 
-    for element in pTexts:
-        if cancelEvent.is_set():
-            break
 
-        # Skip the first iteration (the first index is empty for some reason)
-        if count == 0:
-            count += 1
-            continue
-
-        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages = [
-        {"role": "system", "content" : "Make a short summary of this text: " + element.text}
+    pTexts = soup.find_all("p")
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages = [
+        {"role": "system", "content" : "Make a short summary of this text: " + pTexts[1].text}
         ])
+    summaryGroup += f"Before GPT =======================================================\n\n {pTexts[1].text} \n\n After GPT =======================================================\n\n {completion.choices[0].message.content} \n\n"
+    count = 0
 
-        progressBar['value'] += 10
+    # for element in pTexts:
+    #     if cancelEvent.is_set():
+    #         break
 
-        summaryGroup += f"Before GPT: =======================================================\n\n {element.text} \n\n After GPT: =======================================================\n\n {completion.choices[0].message.content} \n\n"
-        count += 1
-        if count > 10: break 
+    #     # Skip the first iteration (the first index is empty for some reason)
+    #     # if count == 0:
+    #     #     count += 1
+    #     #     continue
+
+    #     completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages = [
+    #     {"role": "system", "content" : "Make a short summary of this text: " + element.text}
+    #     ])
+
+    #     progressBar['value'] += 10
+
+    #     summaryGroup += f"Before GPT: =======================================================\n\n {element.text} \n\n After GPT: =======================================================\n\n {completion.choices[0].message.content} \n\n"
+    #     # count += 1
+    #     # if count > 10: break 
     
     outputSummary.insert(tk.END, summaryGroup)
     progressBar.stop()
+    progressLabel['text'] = ""
+    progressBar.pack_forget()
 
 window = tk.Tk(className="Location Data Entry")
 window.geometry("800x600")
@@ -75,8 +81,7 @@ progressFrame = tk.Frame(master=window)
 progressFrame.pack()
 progressLabel = tk.Label(master=progressFrame, font="Arial")
 progressLabel.pack(pady=10)
-progressBar = ttk.Progressbar(progressFrame, mode='determinate', length=280)
-progressBar.pack()
+progressBar = ttk.Progressbar(progressFrame, mode='indeterminate', length=280)
 
 outputFrame = tk.Frame(master=window)
 outputFrame.pack()
